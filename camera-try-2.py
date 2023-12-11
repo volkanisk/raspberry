9ef6f6fef1da94c79ede2da49d730405a87aa64e
volkanisk <visik18@ku.edu.tr>
2023-12-12 01:17:32 +0300

    picamera 2

--- camera-try-2.py
+++ camera-try-2.py
@@ -1,8 +1,9 @@
-from picamera import PiCamera
-from time import sleep
-
-camera = PiCamera()
-camera.start_preview()
-sleep(5)
-camera.capture('/home/pi/Desktop/image.jpg')
-camera.stop_preview()
+from picamera2 import Picamera2, Preview
+import time
+picam2 = Picamera2()
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.QTGL)
+picam2.start()
+time.sleep(2)
+picam2.capture_file("test.jpg")
