8ad83ccac7c12cb714417fe4a5b5e7fe2944a0d1
volkanisk <visik18@ku.edu.tr>
2024-01-05 14:43:03 +0300

    small update

--- toesptry.py
+++ toesptry.py
@@ -5,16 +5,10 @@ import time
 esp8266_ip = "192.168.1.11"
 esp8266_port = 8080
 
-def on(s):
+def stop(s):
       # Send commands or data
-      command = "ON ON ON ON"  # Example command
+      command = "STOP STOP STOP STOP STOP STOP STOP STOP"  # Example command
       s.sendall(command.encode())
-
-
-def off(s):
-    # Send commands or data
-    command = "OFF OFF OFF OFF"  # Example command
-    s.sendall(command.encode())
 
 def listen(s):
   response = s.recv(1024)
@@ -30,10 +24,7 @@ def listen(s):
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
   s.connect((esp8266_ip, esp8266_port))
   for i in range(10):
-    on(s)
-    # listen(s)
-    time.sleep(1.1)
-    off(s)
+    stop(s)
     # listen(s)
     time.sleep(1.1)
 
