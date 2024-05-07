2a9b090f69bf137b67289f149ae5aa58b4bdcaf9
Batuhan Arat <71669736+batuhanarat@users.noreply.github.com>
2024-05-07 18:46:29 +0300

    Update connectionControl.py

--- connectionControl.py
+++ connectionControl.py
@@ -7,7 +7,7 @@ class ConnectionControl():
     def __init__(self, user_id):
        # self.url = "http://18.206.230.56:5002"
        # self.url = " http://localhost:5002"
-        self.url = "http://54.237.210.151:5002"
+        self.url = "http://54.237.210.151:5004"
         self.plant_exists = True
         self.user_id = user_id
         return
@@ -61,6 +61,7 @@ class ConnectionControl():
         }
         response = requests.post(url, json=payload, headers=headers)
         return response
+        
     def send_images(self,image_array):
         url = f"{self.url}/plantImages"
         files = []
