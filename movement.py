20c1fd952efa9d273d4abdd23c968ebf7fcc038d
volkanisk <visik18@ku.edu.tr>
2024-05-08 19:58:08 +0300

    try

---
+++ movement.py
@@ -0,0 +1,23 @@
+from motorControl import MotorControl
+
+
+
+
+esp_ip = '192.168.43.41'
+motor_controller = MotorControl(esp_ip=esp_ip)
+
+
+image_array = []
+for i in range(3):
+    motor_controller.actuator("run")
+    motor_controller.sleep(1)
+    motor_controller.actuator("stop")
+    motor_controller.sleep(10)
+
+
+motor_controller.actuator("terminate")
+
+
+
+
+
