4a56d8cf80f0182c9e0a3fca9ef4d309a75d6e4c
volkanisk <visik18@ku.edu.tr>
2024-05-11 22:09:05 +0300

    small changes

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,26 +1,28 @@
 from flask import Flask, render_template, Response
+from picamera2 import Picamera2
+from PIL import Image
 
 
-class Camera(object):
-    def __init__(self):
-        # Initialize your camera here
-        # For example, if you're using picamera:
-        from picamera import PiCamera
-        self.camera = PiCamera()
+def get_image(piCam):
+    frame = piCam.capture_array()
+    pil_image = Image.fromarray(frame)
+    pil_image = pil_image.convert('RGB')
+    pil_image = pil_image.resize((128,128))
+    return pil_image
 
-    def get_frame(self):
-        # Capture a frame from the camera and return it as a JPEG-encoded byte string
-        from io import BytesIO
-        stream = BytesIO()
-        self.camera.capture(stream, 'jpeg')
-        return stream.getvalue()
 
+piCam = Picamera2()
+#piCam.preview_configuration.main.size=(1280,720) # setting the size
+piCam.preview_configuration.main.size=(128,128) # setting the size
+piCam.preview_configuration.align() # for non-formal size --> normal size automatically
+piCam.configure("preview")  # add the configurations
+piCam.start()
 
 app = Flask(__name__)
 
-def gen(camera):
+def gen():
     while True:
-        frame = camera.get_frame()
+        frame = get_image(piCam)
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -30,7 +32,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(Camera()),
+    return Response(gen(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
