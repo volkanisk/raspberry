5ac400fa4c7c22d26770b3139b18876932100250
volkanisk <visik18@ku.edu.tr>
2024-05-11 22:16:36 +0300

    small changes

--- flask-app/app.py
+++ flask-app/app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 class Camera(object):
     def __init__(self):
         self.piCam = Picamera2()
-        self.piCam.preview_configuration.main.size=(128,128) # setting the size
-        #self.piCam.preview_configuration.main.size = (1536, 864)  # setting the size
+        #self.piCam.preview_configuration.main.size=(128,128) # setting the size
+        self.piCam.preview_configuration.main.size = (1536, 864)  # setting the size
         self.piCam.preview_configuration.align() # for non-formal size --> normal size automatically
         self.piCam.configure("preview")  # add the configurations
         self.piCam.start()
@@ -18,7 +18,7 @@ class Camera(object):
         frame = self.piCam.capture_array()
         pil_image = Image.fromarray(frame)
         pil_image = pil_image.convert('RGB')
-        #pil_image = pil_image.resize((128, 128))
+        pil_image = pil_image.resize((128, 128))
         img_io = BytesIO()
         pil_image.save(img_io, 'JPEG')
         img_io.seek(0)
