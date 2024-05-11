774d3b65cca7a217b56edc779022010b294458ea
volkanisk <visik18@ku.edu.tr>
2024-05-11 22:11:32 +0300

    small changes

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,28 +1,31 @@
 from flask import Flask, render_template, Response
 from picamera2 import Picamera2
 from PIL import Image
-
-
-def get_image(piCam):
-    frame = piCam.capture_array()
-    pil_image = Image.fromarray(frame)
-    pil_image = pil_image.convert('RGB')
-    pil_image = pil_image.resize((128,128))
-    return pil_image
-
-
-piCam = Picamera2()
-#piCam.preview_configuration.main.size=(1280,720) # setting the size
-piCam.preview_configuration.main.size=(128,128) # setting the size
-piCam.preview_configuration.align() # for non-formal size --> normal size automatically
-piCam.configure("preview")  # add the configurations
-piCam.start()
+from io import BytesIO
 
 app = Flask(__name__)
 
-def gen():
+class Camera(object):
+    def __init__(self):
+        self.piCam = Picamera2()
+        self.piCam.preview_configuration.main.size=(128,128) # setting the size
+        self.piCam.preview_configuration.align() # for non-formal size --> normal size automatically
+        self.piCam.configure("preview")  # add the configurations
+        self.piCam.start()
+
+    def get_frame(self):
+        frame = self.piCam.capture_array()
+        pil_image = Image.fromarray(frame)
+        pil_image = pil_image.convert('RGB')
+        pil_image = pil_image.resize((128, 128))
+        img_io = BytesIO()
+        pil_image.save(img_io, 'JPEG')
+        img_io.seek(0)
+        return img_io.read()
+
+def gen(camera):
     while True:
-        frame = get_image(piCam)
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
@@ -32,7 +35,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(gen(),
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
