3b4539b309f058e09d4c36353c86471589ef10b6
volkanisk <visik18@ku.edu.tr>
2024-05-16 14:48:21 +0300

    rotten

--- image_gatherer.py
+++ image_gatherer.py
@@ -22,7 +22,7 @@ piCam.start()
 esp_ip = '192.168.43.41'
 motor_controller = MotorControl(esp_ip=esp_ip)
 
-connection_id = "66447ca01ce89e8351c503ae"
+connection_id = "6645f22dc7e829c179ddfa11"
 connection_controller = ConnectionControl(user_id=connection_id)
 
 
